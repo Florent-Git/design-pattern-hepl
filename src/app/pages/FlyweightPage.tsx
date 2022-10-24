c048a5498b11d567dc5adfbba511876c5808ac41
Florent-Git <florent.ram@gmail.com>
2022-10-24 11:26:13 +0200

    Fixed import errors

--- src/app/pages/FlyweightPage.tsx
+++ src/app/pages/FlyweightPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { FlyweightStats } from "../components/FlyweightStats";
 import { OptimizedWarrior, UnoptimizedWarrior } from "../model/Entities";
-import { useStateArray } from "../utils/Hooks";
 
 export function FlyweightPage() {
     return (
